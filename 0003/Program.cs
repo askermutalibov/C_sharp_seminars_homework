09fd2659ab142db65bc56b12c533a9307696455a
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-15 20:06:43 +0300

    third task is done

---
+++ 0003/Program.cs
@@ -0,0 +1,18 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+
+double a;
+double b;
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.Write("Введите число: ");
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+
+if (a >= b)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(a);
+}
+Console.WriteLine("Максимальное число: ");
+Console.WriteLine(b);
