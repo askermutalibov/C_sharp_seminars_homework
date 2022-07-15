4ed6147e7572395cf5fffd9a91a866a6d41cd461
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-15 16:42:57 +0300

    heading has been added

--- 0001/Program.cs
+++ 0001/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int FirstNumber = 1; // целые числа
+double SecondNumber = 2; // вещественные числа
+Console.WriteLine(int.MaxValue); // макс. значение целого числа для системы
+Console.WriteLine(double.MaxValue); // макс. значение вещественного числа для системы
+*/
