b9009c499841f040d2852cbeddbeea0b1745821b
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-15 18:12:06 +0300

    second task is done

---
+++ 0002/Program.cs
@@ -0,0 +1,42 @@
+﻿// С клавиатуры вводится целое число. Необходимо вывести квадрат числа
+
+int a,b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+
+if (a*a==b) // если равно
+{
+    System.Console.WriteLine("b является квадратом a");
+}
+else
+{
+    if (b*b==a)
+    {
+        System.Console.WriteLine("а является квадратом b");
+    }
+    else
+    {
+        System.Console.WriteLine("Числа не являются квадратами друг друга");
+    }
+} // от этих фигурных скобок можно избавиться, если во вложенных условиях if, then и else есть только одно действие
+
+
+
+
+/*
+if (2*2==4) // полное условие
+{
+     // true
+}
+else
+{
+     // false
+}
+
+if (2*2==4) // неполное условие
+{
+     // true
+}
+*/
