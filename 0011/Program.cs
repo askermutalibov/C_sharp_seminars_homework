c770df141003e462082f9af832d3e7bab14deafc
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-15 18:23:28 +0300

    eleventh task is done

---
+++ 0011/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести на экран 2-ю с конца цифру числа (56 = 5)
+
+int FirstNumber = 4527;
+int SecondNumber = FirstNumber/10;
+int ThirdNumber = FirstNumber/10%10;
+// System.Console.WriteLine(SecondNumber); лишняя итерация
+System.Console.WriteLine(ThirdNumber);
