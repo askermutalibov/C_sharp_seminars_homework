7f1a305bbbabbbd2de9dc608f69800b1a8033665
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-15 17:43:30 +0300

    firsttask is done

--- 0001/Program.cs
+++ 0001/Program.cs
@@ -4,3 +4,39 @@ double SecondNumber = 2; // вещественные числа
 Console.WriteLine(int.MaxValue); // макс. значение целого числа для системы
 Console.WriteLine(double.MaxValue); // макс. значение вещественного числа для системы
 */
+
+/* 
+int FirstNumber = 1658;
+int SecondNumber = FirstNumber * FirstNumber;
+System.Console.WriteLine(SecondNumber);
+*/
+
+/*
+int FirstNumber = 525;
+int SecondNumber = FirstNumber * FirstNumber;
+System.Console.WriteLine($"{FirstNumber}^2={SecondNumber}");
+System.Console.WriteLine("{0}^2={1}", FirstNumber,SecondNumber); // строка форматирования
+System.Console.WriteLine($"{FirstNumber}^2={SecondNumber}"); // строка интерполяции
+*/
+
+/* 
+string? s; // объявление переменной
+s = Console.ReadLine(); // присвоение значения
+System.Console.WriteLine(s);
+*/
+
+/*
+int a;
+int b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
+*/ // алгоритм, который выводит квадрат числа, введённого пользователем в терминал самостоятельно
+
+double a;
+double b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+b=a*a;
+System.Console.WriteLine($"{a}^2={b}");
