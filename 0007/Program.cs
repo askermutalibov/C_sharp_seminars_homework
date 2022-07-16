e0b09aacee15b888ab6bb300547cb1b728ba13cb
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-16 13:35:11 +0300

    seventh task is done

---
+++ 0007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить является ли число чётным.
+
+double Number;
+string ? s = Console.ReadLine();
+Number = Convert.ToDouble(s);
+
+if (Number%2 == 0) // если остаток от деления числа на 2 равен нулю, то число чётное, иначе - нечётное
+{
+    Console.WriteLine("Введённое вами число чётное");
+}
+else
+{
+    Console.WriteLine("Введённое вами число нечётное");
+}
