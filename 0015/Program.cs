85c1abbfdc43ca1f5198a201d1600db813c85469
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-16 18:52:00 +0300

    fifteen task is done

---
+++ 0015/Program.cs
@@ -0,0 +1,16 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет.
+
+int FromKeyBoard;
+Console.WriteLine("Введите целое число");
+string? t = Console.ReadLine();
+FromKeyBoard = Convert.ToInt32(t);
+
+if ((FromKeyBoard)%100 > 0)
+{
+    Console.WriteLine("Третья цифра числа: "); Console.WriteLine((FromKeyBoard)%10);
+}
+else
+{
+    Console.WriteLine("Третья цифра числа отсутствует");// к сожалению алгоритм работает только для трёхзначных чисел
+}
+
