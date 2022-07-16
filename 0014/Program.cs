e75ee76f44d7b59de01c2a42cf7fb34d084b2b6f
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-16 18:40:12 +0300

    fourteen task is done

---
+++ 0014/Program.cs
@@ -0,0 +1,20 @@
+﻿// С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b, если нет, вывести остаток от деления a на b.
+
+double FirstNumber;
+double SecondNumber;
+Console.Write("Введите первое число: ");
+string? s=Console.ReadLine();
+FirstNumber = Convert.ToDouble(s);
+Console.Write("Введите второе число: ");
+string? w=Console.ReadLine();
+SecondNumber = Convert.ToDouble(w);
+
+if ((FirstNumber % SecondNumber)==0)// алгоритм написан так, что 1-е число должно быть больше второго, хотя технически это никак не выделяется, так как 4 не может быть кратно 16, а если всё-таки может, то как не стыдно автору кода (мне)?
+{
+    Console.Write("Число a кратно числу b");
+}
+else
+    {
+        Console.Write("Число a не кратно числу b, остаток от деления числа a на число b равен ");
+        Console.Write((FirstNumber % SecondNumber));
+    }
