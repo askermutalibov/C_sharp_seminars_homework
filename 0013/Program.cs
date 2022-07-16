84b5ca9fbe0214789754466a7d0ad9ce265bf6ae
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-16 18:21:41 +0300

    thirteen task is done

---
+++ 0013/Program.cs
@@ -0,0 +1,11 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+
+int a;
+Console.WriteLine("Введите целое число");
+string? s = Console.ReadLine();
+a = Convert.ToInt32(s);
+do
+{
+    a = (a/10);
+} while ((a%10)>0);
+Console.WriteLine(a); // не разобрался с решением
