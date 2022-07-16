4292402400f9516a7a4bfb1cfa93ef8edd313038
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-16 16:05:17 +0300

    tenth task is done

---
+++ 0010/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры.
+
+double Number;
+Console.Write("Введите число: ");
+string? s = Console.ReadLine();
+Number = Convert.ToDouble(s);
+Console.WriteLine(Number % 10);
