e9ce30ab3b121186f7670b21842c5ad34b528dd2
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-19 20:01:17 +0300

    digit counts in integer is compeleted

---
+++ 0028/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+
+int CountDigits (int N)
+{
+    int k = 0;
+    if (N==0) return 1; // пограничный случай - если вводим 0 в конец алгоритма
+    while (N!=0)
+    {
+        k++;
+        N=N/10;
+    }
+    return k;
+};
+System.Console.WriteLine(CountDigits(142367));
