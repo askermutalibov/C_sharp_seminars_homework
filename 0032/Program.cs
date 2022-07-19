92c04732de30976b0efbcd400b87e16d1dff8e4b
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-19 19:31:13 +0300

    set array and print his elements

---
+++ 0032/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int []t;
+t = new int[8];
+t[0] = 10;
+t[1] = 12;
+for (int i = 0; i < t.Length; i++)
+t[i] = i*10;
+for (int i = 0; i < t.Length; i++)
+System.Console.WriteLine(
+    $"a[{i}]={t[i]} "
+);
