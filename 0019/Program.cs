07b5d4b425bf81d2c08829b63e969cc17592158d
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-19 19:08:52 +0300

    logical claim verified

---
+++ 0019/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y!
+// (X || Y)==!X && !Y
+
+int n = 5;
+System.Console.WriteLine($"n = {n}");
+bool x, y, f1, f2;
+x = true;
+y = true;
+f1 = !(x || y);
+f2 = !x && !y;
+System.Console.WriteLine($"{x} {y} {f1} {f2}");
