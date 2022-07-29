0ec8df3346bc54f251b2f5976639cadc56805150
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-29 18:32:42 +0300

    bugs fixed

--- 0022/Program.cs
+++ 0022/Program.cs
@@ -2,15 +2,15 @@
 
 Console.Write("Введите 5-ти значное число: ");
 string number = Console.ReadLine()!;
-void Task22(string number )
+void Task22(string number)
 {
-    
-    if (number [0] == number [4] && number [1] == number [3])
+
+    if (number[0] == number[4] && number[1] == number[3])
     {
-    Console.WriteLine("Данное число - палиндром");
+        Console.WriteLine("Данное число - палиндром");
     }
     else
-Console.WriteLine("Данное число не палиндром");
- 
+        Console.WriteLine("Данное число не палиндром");
+
 }
 Task22(number);
