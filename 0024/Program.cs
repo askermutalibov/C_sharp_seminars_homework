522ad975dcb40a14e01cb3dad8e1132e49355836
Asker Mutalibov <askermutalibov@aiesec.net>
2022-07-30 18:13:45 +0300

    0024 project (squares of integers from 1 to N) is done

--- 0024/Program.cs
+++ 0024/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+// первая версия (без универсальных рандомных массивов бесконечной величины)
+
+int[] Numbers = new int[100] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 };
+int length = Numbers.Length;
+int index = 0;
+int NumberRoot = (index + 1);
+int SquareCount;
+while (index < (length))
+{
+    SquareCount = (Numbers[(index)] * Numbers[(index)]); // хотел реализовать через функцию Math.Pow, но она работает с вещественными числами типа double, integers игнорирует.
+    System.Console.WriteLine($"Квадрат числа {NumberRoot} равен {SquareCount}");
+    index++;
+    NumberRoot++;
+}
+
