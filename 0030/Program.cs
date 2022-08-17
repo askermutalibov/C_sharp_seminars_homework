c845c0292c5d07f4c9e955ae53997bd5502d88f7
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-17 19:15:44 +0300

    0030 proj is done

--- 0030/Program.cs
+++ 0030/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число, для которого необходимо рассчитать его факториал: ");
+
+string? FactorialString = Console.ReadLine();
+int FactorialOfNumber = Convert.ToInt32(FactorialString);
+int i = 1;
+int Factorial = 1;
+while (i <= FactorialOfNumber)
+{
+    Factorial = Factorial * (i + 1);
+    i++;
+    if (i == FactorialOfNumber)
+    {
+        Console.WriteLine($"факториал числа {FactorialOfNumber} равен {Factorial}");
+    }
+}
