2c1b8e2c52b3e9b9afc989ebfa9936161ae226bb
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-17 19:55:30 +0300

    0031 proj is done

--- 0031/Program.cs
+++ 0031/Program.cs
@@ -1,2 +1,19 @@
 ﻿// Показать кубы чисел, заканчивающихся на чётную цифру
-Console.WriteLine("Hello, World!");
+
+Console.Write("Введите число, которое будет верхним пределом последовательности чисел, для которой нужно вывести на экран кубы чётных чисел из этой последовательности: ");
+
+string? CubesString = Console.ReadLine();
+int CubesInt = Convert.ToInt32(CubesString);
+
+int count = 2;
+int Cube = 0;
+
+while ((count <= CubesInt) && (count % 2 == 0))
+{
+    Cube = (CubesInt * CubesInt * CubesInt);
+    count = (count + 2);
+    if ((count == CubesInt))
+    {
+        Console.WriteLine($"куб числа {CubesInt} равен {Cube}");
+    }
+}
