4e24a45de5c82edcdf8aec616b8a1063c1c9682c
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-18 17:16:55 +0300

    changed some settings in launch.json file for debug initialization

--- 1000_variables/Program.cs
+++ 1000_variables/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// .NET variables
+
+/*
+
+// значимые типы данных (valuable types)
+int a;
+double b;
+a = 4; // просто записывается
+
+// ссылочные типы данных (reference types)
+string c;
+c = "Hello";
+int[] array;
+array = new int[5]; // объявляем массив со ссылкой на место в RAM, которое равняется числу в квадратных скобках (условно 5 битов, например)
+
+*/
+
+// arguments in C#
+
+int a, b;
+a = 1;
+b = 2;
+System.Console.WriteLine($"{a} {b}");
+
+a = b;
+b = a;
+System.Console.WriteLine($"{a} {b}");
+
+a = a + b;
+b = a - b;
+a = a - b;
+System.Console.WriteLine($"{a} {b}");
