bd46f5330aa6cad319d277e0a204bfda46e9d298
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-19 13:11:17 +0300

    0036 proj is done

--- 0036/Program.cs
+++ 0036/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить, присутствует ли в заданном массиве некоторое число
+
+int N = 12;
+int[] a = new int [N];
+Random random = new Random();
+
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = random.Next(-9, 10);
+}
+
+int f = 400;
+a[3] = f;
+int j = 0; // линейный поиск
+while (j < a.Length && a[j] != f)
+{
+    j++;
+}
+
+for (int i = 0; i < a.Length; i++)
+{
+    System.Console.Write($"{a[i],4}"); // выделяем 4 знакоместа (но можно это реализовать просто через пробелы)
+}
+
+if (j == a.Length)
+{
+System.Console.WriteLine("Not Found");
+}
+else System.Console.WriteLine($"\nЭлемент {f} найден на позиции {j} индекса массива"); // \n - new line, добавляет новую линию внутри сообщения
