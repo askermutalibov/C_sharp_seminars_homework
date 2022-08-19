d7d55836670e38e196154ea0f46dc7efcd1fc5c1
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-19 13:28:21 +0300

    0039 proj is done

--- 0039/Program.cs
+++ 0039/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int N = 12;
+int[] a = new int[N];
+Random random = new Random();
+
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = random.Next(-9, 10);
+}
+
+for (int i = 0; i < a.Length; i++)
+{
+    System.Console.Write($"{a[i],4}"); // выделяем 4 знакоместа (но можно это реализовать просто через пробелы)
+}
+
+int s = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    if ((i % 2) != 0)
+        s = s + a[i];
+        System.Console.WriteLine($"сумма чисел массива, расположенных на нечётной позиции равна {s}");
+}
+// System.Console.WriteLine(s);
