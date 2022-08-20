1dfca3fb7b6e7479677ab3dd0d3c78204296e7fc
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-20 17:46:41 +0300

    0039 proj code edited

--- 0039/Program.cs
+++ 0039/Program.cs
@@ -14,11 +14,11 @@ for (int i = 0; i < a.Length; i++)
     System.Console.Write($"{a[i],4}"); // выделяем 4 знакоместа (но можно это реализовать просто через пробелы)
 }
 
+Console.WriteLine(); Console.WriteLine();
 int s = 0;
 for (int i = 0; i < a.Length; i++)
 {
     if ((i % 2) != 0)
         s = s + a[i];
-        System.Console.WriteLine($"сумма чисел массива, расположенных на нечётной позиции равна {s}");
 }
-// System.Console.WriteLine(s);
+System.Console.WriteLine($"сумма чисел массива, расположенных на нечётной позиции равна {s}");
