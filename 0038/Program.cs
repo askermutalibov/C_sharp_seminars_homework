4e5f789c49421108a3934685647f70ab584515b4
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-20 14:29:34 +0300

    0038 proj is done

--- 0038/Program.cs
+++ 0038/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] arrOneTwoThree;
+int length = 123; 
+arrOneTwoThree = new int[length];
+int countElements = 0;
+Random arrOneTwoThreeCreate = new Random();
+
+for (int i = 0; i < length; i++)
+{
+    arrOneTwoThree[i] = arrOneTwoThreeCreate.Next(1, (length + 1));
+}
+
+for (int i = 0; i < length; i++)
+{
+    if (arrOneTwoThree[i] >= 10 && arrOneTwoThree[i] <= 99)
+    {
+        countElements++;
+    }
+}
+Console.WriteLine($"Количество элементов в массиве, лежащих в отрезке от 10 включительно до 99 включительно составляет {countElements}");
