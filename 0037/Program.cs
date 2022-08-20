99f9edba6701970544a9bfb68755f09e1a846a0b
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-20 14:03:21 +0300

    0037 proj is done

--- 0037/Program.cs
+++ 0037/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] thirdDigitNumbersArray;
+int length = 900; // последовательность чисел от 100 включительно до 999 включительно
+thirdDigitNumbersArray = new int[length];
+int sumTwo = 0;
+int sumNotTwo = 0;
+Random createArray = new Random();
+
+for (int i = 0; i < length; i++)
+{
+    thirdDigitNumbersArray[i] = createArray.Next(100, (length + 1));
+}
+Console.WriteLine($"\nЧётные элементы массива указаны ниже:"); // добавление пустой строки для обрамления результата и удобства считывания последовательности чётных чисел (и нечётных в частности) в массиве
+for (int i = 0; i < length; i++)
+{
+    if (thirdDigitNumbersArray[i] % 2 == 0)
+    {
+        Console.Write($"{thirdDigitNumbersArray[i],4}");
+        sumTwo++;
+    }
+}
+Console.WriteLine(); Console.WriteLine($"\nНечётные элементы массива указаны ниже:");
+for (int i = 0; i < length; i++)
+{
+    if (thirdDigitNumbersArray[i] % 2 != 0)
+    {
+        Console.Write($"{thirdDigitNumbersArray[i],4}");
+        sumNotTwo++;
+    }
+}
+Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); // обрамление результатов алгоритма
+Console.WriteLine($"Количество чётных чисел: {sumTwo}");
+Console.WriteLine($"Количество нечётных чисел: {sumNotTwo}");
