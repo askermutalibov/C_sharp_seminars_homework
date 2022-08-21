deffa507acfe7af3a390b34e61d562b22cfaca24
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-21 15:34:36 +0300

    0044 proj is done

--- 0044/Program.cs
+++ 0044/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+
+int n = 123456789;
+Console.WriteLine();
+Console.WriteLine($"Число {n} в десятичной системе счисления будет записано как {DecimalToBinary(n)} в двоичной системе счисления");
+
+string DecimalToBinary(int n)
+{
+    string s = "";
+    while (n != 0)
+    {
+        s = (n % 2).ToString() + s;
+        n /= 2; // n = n/2
+    }
+    return s;
+}
