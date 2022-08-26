ef77816a73a6e987c498b4dcfaf8e2e1507d9156
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-08-26 17:28:56 +0300

    0048_1 proj is done

--- 0048_1/Program.cs
+++ 0048_1/Program.cs
@@ -1,3 +1,28 @@
 ﻿// Показать двумерный массив размером m x n заполненный целыми случайными числами
-Console.WriteLine("Hello, World!");
 
+int N = 128;
+int M = 32;
+int[,] a;
+a = new int[N, M]; // создание массива
+
+// инициализация случайными числами
+Random random = new Random();
+
+for (int i = 0; i < a.GetLength(0); i++) // вместо for (int j = 0; j < M; j++)
+{
+    for (int j = 0; j < a.GetLength(1); j++) // GetLength считает измерения массива
+    {
+        a[i, j] = random.Next(1, 96);
+    }
+}
+
+// вывод массива на экран
+
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++) // GetLength считает измерения массива
+    {
+        System.Console.Write($"{a[i, j], 0}  ");
+    }
+    System.Console.WriteLine();
+}
