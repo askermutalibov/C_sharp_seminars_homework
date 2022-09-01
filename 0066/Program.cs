de40138a6ca229a6c7508a73c943d472e2b5e8f1
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-09-01 11:29:45 +0300

    0066 proj is done

--- 0066/Program.cs
+++ 0066/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести на экран числа от 1 до N (N задано) без использования циклов 
+
+
+void Loop(int i, int N)
+{
+    Console.Write($"{i, 5}");
+    if (i < N)
+    {
+        Loop(i + 1, N);
+    }
+}
+
+Loop(1, 10);
