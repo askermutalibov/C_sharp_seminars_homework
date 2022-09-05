68fedc206b78a804a461631d760a167c31fe26f2
Asker Mutalibov <mutalibov.asker@mail.ru>
2022-09-05 19:23:58 +0300

    0070 proj is done

--- 0070/Program.cs
+++ 0070/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму цифр числа
+
+int CountSum(int integer)
+{
+   if (integer == 0) return 0;
+   else if (integer > 0 && integer < 10) return integer;
+   else return (integer % 10) + CountSum(integer / 10);
+}
+
+Console.WriteLine(CountSum(123581321));
